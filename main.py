efe76673c5d5138bed46050590970b07883aa506
Shavem <73134915+Shavem@users.noreply.github.com>
2022-02-27 12:09:54 -0600

    created automated sheet download handler

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-import matplotlib.pyplot as plt
-# import numpy as np
-# import pandas as pd
-# import seaborn as sns
+import os
+import time
+import shutil
 
 
+# Set working directory
+directory = "C:/Users/smrit/Downloads"
+os.chdir(directory)
 
-# Make a graph
+def getDownloads():
+    files = filter(os.path.isfile, os.listdir())
+    files = [os.path.join(f) for f in files]
+    files.sort(key=lambda x: os.path.getmtime(x))
+    return files
+
+og = getDownloads()[-1]
+
+while True:
+    time.sleep(5)
+    new = getDownloads()[-1]
+    if (og != new):
+        if (new.startswith("M3 tables")):
+            print(f"{new} has been downloaded")
+            os.rename(f"{directory}/{new}", f"{directory}/table.csv")
+            os.remove("C:/Users/smrit/PycharmProjects/M3/cities/table.csv")
+            shutil.move(f"{directory}/table.csv", f"C:/Users/smrit/PycharmProjects/M3/cities/table.csv")
