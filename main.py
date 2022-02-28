136ca74050abee064a7f4b564fecb4e9156e43c3
Shavem <73134915+Shavem@users.noreply.github.com>
2022-02-27 18:04:37 -0600

    made pretty

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
+# This script will grab any of our downloaded google sheets and automatically import them into our Python project so we can more easily collaborate.
+
+# Module imports
 import os
 import time
 import shutil
+import getpass
 
 
 # Set working directory
-directory = "C:/Users/smrit/Downloads"
+currentwd = os.getcwd().replace('\\', '/')
+directory = f"C:/Users/{getpass.getuser()}/Downloads"
 os.chdir(directory)
 
+# Get downloads sorted by modified date
 def getDownloads():
     files = filter(os.path.isfile, os.listdir())
     files = [os.path.join(f) for f in files]
     files.sort(key=lambda x: os.path.getmtime(x))
     return files
 
+# Get the name of the latest file
 og = getDownloads()[-1]
 
 while True:
+    # Wait for 5 seconds to decrease overhead
     time.sleep(5)
     new = getDownloads()[-1]
+    # If new file exists
     if (og != new):
+        # If the new file starts with M3 tables (the name of our Google sheet)
         if (new.startswith("M3 tables")):
+            # Import it into our project
             os.rename(f"{directory}/{new}", f"{directory}/table.csv")
-            os.remove("C:/Users/smrit/PycharmProjects/M3/cities/table.csv")
-            shutil.move(f"{directory}/table.csv", f"C:/Users/smrit/PycharmProjects/M3/cities/table.csv")
+            os.remove(f"{currentwd}/cities/table.csv")
+            shutil.move(f"{directory}/table.csv", f"{currentwd}/cities/table.csv")
             print(f"tables.csv has been updated")
 
